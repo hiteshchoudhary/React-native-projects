847577bf713692d45647cede6c5cc3bf8ef1e1b0
Abhinav Kumar <68495520+abhinav2712@users.noreply.github.com>
2023-03-17 00:37:07 +0530

    Optimized Code for getting  creating password
    In createPassword() function, instead of using Math.round(Math.random()*characters.length),  I used Math.floor(Math.random()*characters.length) to avoid potentially accessing an out-of-bounds character index, making it more optimized and faster than the previous code.
    I even tested it, it runs perfectly and relatively faster.
    Happy Hacking

--- passwordGenerator03/App.tsx
+++ passwordGenerator03/App.tsx
@@ -55,7 +55,7 @@ export default function App() {
   const createPassword = (characters: string, passwordLength: number) => {
     let result = ''
     for (let i = 0; i < passwordLength; i++) {
-      const characterIndex = Math.round(Math.random() * characters.length)
+      const characterIndex = Math.floor(Math.random()*characters.length)
       result += characters.charAt(characterIndex)
     }
     return result
